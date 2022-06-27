de522f210bc725c61d9db4c92ee6cd21b0abde6a
Lucas0105 <dnjswo105@gmail.com>
2022-06-27 18:04:37 +0900

    feat : connect jpa with mysql (#1)

---
+++ jpa-connect/jpa-connect/src/test/java/com/example/jpaconnect/JpaConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
